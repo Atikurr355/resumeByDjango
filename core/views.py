ea36f880749a0932120542310846cacab397924e
Atikurr355 <mdatikurr355@gmail.com>
2021-11-13 13:49:26 +0530

    admin added, homeData added and retrive

--- core/views.py
+++ core/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-
+from .models import Home
 
 # Create your views here.
 def home(request):
+    homeData = Home.objects.all()[0]
     context = {
-        'home':"active"
+        'home':"active",
+        'homeData': homeData
     }
     return render(request, 'core/home.html',context)
 
