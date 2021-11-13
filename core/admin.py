d7cb92471283a7444fdced50becbbd459ea31f0c
Atikurr355 <mdatikurr355@gmail.com>
2021-11-13 14:30:36 +0530

    tinyMCE added

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Home
 # Register your models here.
-admin.site.register(Home)
+
+class HomeAdmin(admin.ModelAdmin):
+    class Media:
+        css = {
+            "all": ("core/css/core.css",)
+        }
+
+        js = ("core/js/home.js",)
+
+admin.site.register(Home,HomeAdmin)
